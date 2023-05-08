cf4e7ffb3003179d4d21f393b9e104a35c584124
Julian Doherty <madlep@madlep.com>
2023-05-08 15:47:23 +1000

    initial commit

---
+++ lib/zenum.ex
@@ -0,0 +1,18 @@
+defmodule Zenum do
+  @moduledoc """
+  Documentation for `Zenum`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zenum.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zenum_test.exs
@@ -0,0 +1,8 @@
+defmodule ZenumTest do
+  use ExUnit.Case
+  doctest Zenum
+
+  test "greets the world" do
+    assert Zenum.hello() == :world
+  end
+end
