dcfae10e48c497323e8d43dd249787fba149963f
Julian Doherty <madlep@madlep.com>
2023-05-11 12:33:37 +1000

    list stack state

--- lib/bench/transforms/zenum_list_stack_state.ex
+++ lib/bench/transforms/zenum_list_stack_state.ex
@@ -7,9 +7,9 @@ defmodule Bench.Transforms.ZenumListStackState do
 
   defp z1_filter_init(data), do: data
 
-  defp z1_filter([value | values]) do
+  defp z1_filter([value | values] = data) do
     if value.reference == "REF3" do
-      {value, values}
+      data
     else
       z1_filter(values)
     end
@@ -19,11 +19,11 @@ defmodule Bench.Transforms.ZenumListStackState do
 
   defp z2_flat_map_init(data), do: [[] | z1_filter_init(data)]
 
-  defp z2_flat_map([[value | values] | state]), do: {value, [values | state]}
+  defp z2_flat_map([[value | values] | state]), do: [value | [values | state]]
 
   defp z2_flat_map([[] | state]) do
     case z1_filter(state) do
-      {value, new_state} -> z2_flat_map([value.events | new_state])
+      [value | new_state] -> z2_flat_map([value.events | new_state])
       :done -> :done
     end
   end
@@ -32,9 +32,9 @@ defmodule Bench.Transforms.ZenumListStackState do
 
   defp z3_filter(state) do
     case z2_flat_map(state) do
-      {value, new_state} ->
+      [value | new_state] = data ->
         if value.included? do
-          {value, new_state}
+          data
         else
           z3_filter(new_state)
         end
@@ -48,7 +48,7 @@ defmodule Bench.Transforms.ZenumListStackState do
 
   defp z4_map(state) do
     case z3_filter(state) do
-      {value, new_state} -> {{value.event_id, value.parent_id}, new_state}
+      [value | new_state] -> [{value.event_id, value.parent_id} | new_state]
       :done -> :done
     end
   end
@@ -59,7 +59,7 @@ defmodule Bench.Transforms.ZenumListStackState do
 
   defp z5_take([values, n | state]) do
     case z4_map(state) do
-      {value, new_state} -> z5_take([[value | values], n + 1 | new_state])
+      [value | new_state] -> z5_take([[value | values], n + 1 | new_state])
       :done -> :lists.reverse(values)
     end
   end
