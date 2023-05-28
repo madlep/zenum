0904072e7d14eb55a094c99f914fbcba37eb4276
Julian Doherty <madlep@madlep.com>
2023-05-28 17:04:05 +1000

    make arg names clearer

--- lib/bench/transforms/hand_optimised.ex
+++ lib/bench/transforms/hand_optimised.ex
@@ -1,28 +1,30 @@
 defmodule Bench.Transforms.HandOptimised do
   def run(data), do: do_opt([], 20, [], data)
 
-  # take finished - got 20 values
-  defp do_opt(z5_acc, 0, _z2_acc, _z0_data), do: :lists.reverse(z5_acc)
+  # take finished (take_n == 0) OR end of data (flatmap_buffer == [] AND data == [])
+  defp do_opt(take_acc, take_n, flatmap_buffer, data)
+       when take_n == 0 or (flatmap_buffer == [] and data == []),
+       do: :lists.reverse(take_acc)
 
-  # take finished - no more data
-  defp do_opt(z5_acc, _z5_n, [], []), do: :lists.reverse(z5_acc)
+  # flatmap + filter true + map when flatmap_buffer is empty
+  # flatmap buffer is empty, and there is more data, AND the head of that data reference matches (filter), AND there are events on it (map)
+  defp do_opt(take_acc, take_n, [], [%{reference: :REF3, events: flatmap_buffer} | data]),
+    do: do_opt(take_acc, take_n, flatmap_buffer, data)
 
-  # take + map + filter true + flat_map when flat_map buffer is empty and needs new value from data
-  defp do_opt(z5_acc, z5_n, [], [%{reference: :REF3, events: z2_acc} | z0_data]),
-    do: do_opt(z5_acc, z5_n, z2_acc, z0_data)
-
-  # take + map + filter false when flat_map buffer is empty and needs new value from data
-  defp do_opt(z5_acc, z5_n, [], [_ | z0_data]), do: do_opt(z5_acc, z5_n, [], z0_data)
+  # flatmap + filter false when flatmap_buffer is empty
+  # skip value, and get next one
+  defp do_opt(take_acc, take_n, [], [_ | data]), do: do_opt(take_acc, take_n, [], data)
 
   # take + flat_map + filter true + map when flat map buffer has values
   defp do_opt(
-         z5_acc,
-         z5_n,
-         [%{included?: true, event_id: event_id, parent_id: parent_id} | z2_acc],
-         z0_data
+         take_acc,
+         take_n,
+         [%{included?: true, event_id: event_id, parent_id: parent_id} | flatmap_buffer],
+         data
        ),
-       do: do_opt([{event_id, parent_id} | z5_acc], z5_n - 1, z2_acc, z0_data)
+       do: do_opt([{event_id, parent_id} | take_acc], take_n - 1, flatmap_buffer, data)
 
   # take + flat_map + filter false when flat map buffer has values
-  defp do_opt(z5_acc, z5_n, [_ | z2_acc], z0_data), do: do_opt(z5_acc, z5_n, z2_acc, z0_data)
+  defp do_opt(take_acc, take_n, [_ | flatmap_buffer], data),
+    do: do_opt(take_acc, take_n, flatmap_buffer, data)
 end
