8602ae579d634c9a3992d81d0c4140ce4fdbd52c
Julian Doherty <julian@juliandoherty.com>
2025-05-21 18:16:26 +0200

    test some more variants

--- test/zenum_test.exs
+++ test/zenum_test.exs
@@ -17,10 +17,43 @@ defmodule ZenumTest do
     assert actual == [2, 4, 6, 2, 4, 6]
   end
 
-  # test "simple functions" do
+  defp double(x), do: x * 2
+  defp lte6(x), do: x <= 6
+
+  test "simple pipe local functions" do
+    input = [1, 2, 3, 4, 1, 2, 3]
+
+    actual =
+      input
+      |> Zenum.from_list()
+      |> Zenum.map(&double/1)
+      |> Zenum.filter(&lte6/1)
+      |> Zenum.to_list()
+
+    assert actual == [2, 4, 6, 2, 4, 6]
+  end
+
+  # TODO doesn't compile yet. Need to store non-literal callback functions in state args
+  # test "simple functions variable" do
+  #   map_f = fn x -> x * 2 end
+  #   filter_f = fn x -> x <= 6 end
+
   #   actual =
-  #     Zenum.filter(Zenum.map([1, 2, 3, 4, 1, 2, 3], fn x -> x * 2 end), fn x -> x <= 6 end)
+  #     Zenum.to_list(
+  #       Zenum.filter(Zenum.map(Zenum.from_list([1, 2, 3, 4, 1, 2, 3]), map_f), filter_f)
+  #     )
 
   #   assert actual == [2, 4, 6, 2, 4, 6]
   # end
+
+  test "simple functions inline" do
+    actual =
+      Zenum.to_list(
+        Zenum.filter(Zenum.map(Zenum.from_list([1, 2, 3, 4, 1, 2, 3]), fn x -> x * 2 end), fn x ->
+          x <= 6
+        end)
+      )
+
+    assert actual == [2, 4, 6, 2, 4, 6]
+  end
 end
